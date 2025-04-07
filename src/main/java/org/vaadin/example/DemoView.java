a374f8012868f9b1248a0e15b75bfc634e9a2c2b
AB <a.bierler@xdev-software.de>
2025-04-07 13:47:37 +0200

    Add Router Links

---
+++ src/main/java/org/vaadin/example/DemoView.java
@@ -0,0 +1,16 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * The main view contains a text field for getting the user name and a button
+ * that shows a greeting message on a new line.
+ */
+@Route("demo")
+public class DemoView extends VerticalLayout {
+
+    public DemoView() {
+
+    }
+}
--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -7,6 +7,7 @@ import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
 
 /**
  * The main view contains a text field for getting the user name and a button
@@ -16,27 +17,6 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addClassName("bordered");
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello", e -> {
-            add(new Paragraph(greetService.greet(textField.getValue())));
-        });
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in
-        // styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        add(new RouterLink("TEST", DemoView.class));
     }
 }
